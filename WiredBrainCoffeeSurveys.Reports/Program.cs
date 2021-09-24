ddb1d1ee2894d893fb61156196575b4f300ad354
Michael Robards <mikerobards@icloud.com>
2021-09-24 17:01:11 -0400

    add overallScore

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -6,11 +6,15 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            // Calculated Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed; 
             double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
 
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
             Console.WriteLine($"Response Percentage: {responseRate}");
             Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
+            Console.WriteLine($"Overall Score: {overallScore}");
         }
     }
 }
