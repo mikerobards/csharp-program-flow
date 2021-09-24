cbfe1fa07d098e091a6dc4f23813c18a63c93c6e
Michael Robards <mikerobards@icloud.com>
2021-09-24 16:28:00 -0400

    add variables and print results

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -6,7 +6,11 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed; 
+            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+
+            Console.WriteLine($"Response Percentage: {responseRate}");
+            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
         }
     }
 }
